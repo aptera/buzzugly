31767b77e38ea1fcade6dfcd214dfa3faaacc39d
Jon Fazzaro <jfazzaro@apterainc.com>
2017-03-22 15:17:51 -0400

    Add project files.

---
+++ sustainable/FizzBuzzPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sustainable
+{
+    class FizzBuzzPlayer
+    {
+        public string Play(int v)
+        {
+            if (v % 15 == 0)
+                return "fizzbuzz";
+
+            if (v % 3 == 0)
+                return "fizz";
+
+            if (v % 5 == 0)
+                return "buzz";
+
+            return v.ToString();
+        }
+    }
+}
---
+++ sustainable/The_fizz_buzz_player.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sustainable
+{
+    [TestFixture]
+    public class The_fizz_buzz_player
+    {
+        FizzBuzzPlayer _player;
+
+        [TestCase(1, ExpectedResult = "1", TestName = "given 1, returns 1")]
+        [TestCase(2, ExpectedResult = "2", TestName = "given 2, returns 2")]
+        [TestCase(3, ExpectedResult = "fizz", TestName = "given 3, returns fizz")]
+        [TestCase(5, ExpectedResult = "buzz", TestName = "given 5, returns buzz")]
+        [TestCase(9, ExpectedResult = "fizz", TestName = "given 9, returns fizz")]
+        [TestCase(10, ExpectedResult = "buzz", TestName = "given 10, returns buzz")]
+        [TestCase(15, ExpectedResult = "fizzbuzz", TestName = "given 15, returns fizzbuzz")]
+        [TestCase(90, ExpectedResult = "fizzbuzz", TestName = "given 90, returns fizzbuzz")]
+        public string play(int i)
+        {
+            Arrange();
+            return _player.Play(i);
+        }
+
+        void Arrange()
+        {
+            _player = new FizzBuzzPlayer();
+        }
+    }
+}
